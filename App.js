af33ef2bb3ac9631651e2f4ed7aa63309b0f7be9
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-04-08 22:08:42 +0530

    header working,bug found major with react-navigation-button

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { enableScreens } from 'react-native-screens';
+
+
+// FONT
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+enableScreens()
+
+import MealNavigator from "./navigation/MealsNavigator"
 
 export default function App() {
+
+  const fetchFonts = async () => {
+    await Font.loadAsync({
+      "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+      "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+    });
+  };
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err) => console.log(err)}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MealNavigator />
   );
 }
 
