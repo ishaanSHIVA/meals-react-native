8b85e09fcb92a57fc670acb5289fee409f12a3ba
ishaanSHIVA <ishaanshiva1@gmail.com>
2022-04-12 11:25:50 +0530

    redux , favs problem

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react'
-import { enableScreens } from 'react-native-screens';
-
+import { useState } from "react";
+import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 // FONT
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 
-enableScreens()
+enableScreens();
 
-import MealNavigator from "./navigation/MealsNavigator"
+import MealNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
+
+const fetchFonts = async () => {
+  await Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
-
-  const fetchFonts = async () => {
-    await Font.loadAsync({
-      "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-      "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-    });
-  };
   const [fontLoaded, setFontLoaded] = useState(false);
 
   if (!fontLoaded) {
@@ -31,15 +38,8 @@ export default function App() {
     );
   }
   return (
-    <MealNavigator />
+    <Provider store={store}>
+      <MealNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
